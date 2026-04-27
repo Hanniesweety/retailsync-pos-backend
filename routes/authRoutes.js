1478b56868e66c4e9aed031c4258966c3c2c97af
Hanniesweety <hanniesweetyh@gmail.com>
2026-04-27 12:23:51 +0530

    removed dummy routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-console.log("NEW AUTH ROUTE LOADED");
+
 const {
   registerUser,
   loginUser
 } = require("../controllers/authController");
 
-// ✅ USE REAL CONTROLLERS
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
